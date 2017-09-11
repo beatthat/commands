9677c6a39507c86d3bc5931dda73485e3d41a3a7
larrykirschner <larrykirschner@gmail.com>
2017-09-10 22:39:31 -0700

    0.0.4

--- src/commands/NotificationCommand.cs
+++ src/commands/NotificationCommand.cs
@@ -3,6 +3,11 @@ using BeatThat.Service;
 
 namespace BeatThat.App
 {
+	/// <summary>
+	/// Command that registers itself to execute 
+	/// when a specific notification type is dispatched by the NotificationBus,
+	/// with that notification type left as a public property so that it can be set via the unity editor.
+	/// </summary>
 	abstract public class NotificationCommand<ArgType> : NotificationCommandBase<ArgType>
 	{
 		public NotificationType m_notificationType;
@@ -12,6 +17,12 @@ namespace BeatThat.App
 		
 	abstract public class NotificationCommand : NotificationCommand<Notification> {}
 
+
+	/// <summary>
+	/// Command that registers itself to execute 
+	/// when a specific notification type is dispatched by the NotificationBus,
+	/// with that notification type provided by the subclass (usually just hard coded).
+	/// </summary>
 	abstract public class NotificationCommandBase<ArgType> : Subcontroller, AutoInitService
 	{
 		public void InitService (Services services)
