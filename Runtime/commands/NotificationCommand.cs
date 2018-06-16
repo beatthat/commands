1493cea599d151d3c1f6140d8bb95c57b8b77fa9
Larry Kirschner <larrykirschner@gmail.com>
2018-06-16 09:58:00 -0700

    namespaced

--- Runtime/commands/NotificationCommand.cs
+++ Runtime/commands/NotificationCommand.cs
@@ -1,21 +1,34 @@
-﻿using System;
+using System;
+using BeatThat.Controllers;
+using BeatThat.Notifications;
 using BeatThat.Service;
 
-namespace BeatThat.App
+namespace BeatThat.Commands
 {
-	/// <summary>
-	/// Command that registers itself to execute 
-	/// when a specific notification type is dispatched by the NotificationBus,
-	/// with that notification type left as a public property so that it can be set via the unity editor.
-	/// </summary>
-	abstract public class NotificationCommand<ArgType> : NotificationCommandBase<ArgType>
+    /// <summary>
+    /// Command that registers itself to execute 
+    /// when a specific notification type is dispatched by the NotificationBus,
+    /// with that notification type left as a public property so that it can be set via the unity editor.
+    /// </summary>
+    abstract public class NotificationCommand<ArgType> : NotificationCommandBase<ArgType>
 	{
 		public NotificationType m_notificationType;
 
 		override public string notificationType { get { return m_notificationType != null? m_notificationType.notificationType: null; } }
 	}
 		
-	abstract public class NotificationCommand : NotificationCommand<Notification> {}
+	abstract public class NotificationCommand : NotificationCommand<Notification> 
+    {
+        override public void Execute(Notification n)
+        {
+            Execute();
+        }
+
+        virtual public void Execute()
+        {
+            throw new NotImplementedException("must override either Execute() (with no args) or Execute(Notification)");
+        }
+    }
 
 
 	/// <summary>
@@ -25,10 +38,12 @@ namespace BeatThat.App
 	/// </summary>
 	abstract public class NotificationCommandBase<ArgType> : Subcontroller, AutoInitService
 	{
+		#pragma warning disable 618
 		public void InitService (Services services)
 		{
 			Bind();
 		}
+		#pragma warning restore 618
 
 		abstract public string notificationType { get; }
 
@@ -51,3 +66,4 @@ namespace BeatThat.App
 	abstract public class NotificationCommandBase : NotificationCommandBase<Notification> {}
 
 }
+
---
+++ Runtime/commands/RegisterCommandAttribute.cs
@@ -0,0 +1,28 @@
+using System;
+using BeatThat.Service;
+
+namespace BeatThat.Commands
+{
+    /// <summary>
+    /// Marks a Command to be autowire registered to Services.
+    /// For now, functions in every way identically to RegisterService,
+    /// but since registering commands is so common wanted at least the syntactic sugar.
+    /// 
+    /// @param serviceInterface (optional) interface used to retrieve the Command from services, 
+    /// when left null, registers to the concrete type.
+    /// 
+    /// 
+    /// @param priority (optional/default 0) used in the case that multiple implementations 
+    /// 	have autowireservice attributes for the same service interface.
+    /// 	Resolves to the implementation with the LOWEST priority value.
+    /// 
+    /// @param proxyInterfaces (optional) list of alternative interfaces that can be used to locate the service
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class RegisterCommandAttribute : RegisterServiceAttribute 
+	{
+		public RegisterCommandAttribute(Type serviceInterface = null, int priority = 0, Type[] proxyInterfaces = null) : base(serviceInterface, proxyInterfaces, priority) {}
+	}
+}
+
